4a9261654d7a484470e7a8dd3620245a4841de39
Saveliy <ya.savchik2000@mail.ru>
2023-11-19 23:42:55 +0400

    Создание структуры проекта

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Mailing(models.Model):
+    """Модель таблицы - товары"""
+    time = models.DateTimeField(verbose_name='Время рассылки')
+    periodisity = models.CharField(max_length=150, verbose_name='Периодичность')
+    status = models.CharField(max_length=10, verbose_name='Статус рассылки')
+    client = models.ManyToManyField('Client', verbose_name='Клиент')
+    message = models.ForeignKey('Message', on_delete=models.CASCADE, verbose_name='Сообщение')
+
+    def __str__(self):
+        return f'{self.periodisity}'
+
+    class Meta:
+        """Класс отображения метаданных"""
+        verbose_name = 'Рассылка'
+        verbose_name_plural = 'Рассылки'
+
+
+class Client(models.Model):
+    """Модель таблицы - товары"""
+    email = models.EmailField(max_length=150, verbose_name='Контактная почта')
+    full_name = models.CharField(max_length=150, verbose_name='ФИО')
+    comment = models.TextField(max_length=500, **NULLABLE, verbose_name='Коментарий')
+
+    def __str__(self):
+        return f'{self.full_name}'
+
+    class Meta:
+        """Класс отображения метаданных"""
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+
+class Message(models.Model):
+    """Модель таблицы - товары"""
+    subject = models.CharField(max_length=150, verbose_name='Тема письма')
+    body = models.TextField(max_length=1000, **NULLABLE, verbose_name='Тело письма')
+
+    def __str__(self):
+        return f'{self.subject}'
+
+    class Meta:
+        """Класс отображения метаданных"""
+        verbose_name = 'Письмо'
+        verbose_name_plural = 'Письма'
+
+
+class Log(models.Model):
+    """Модель таблицы - товары"""
+    time = models.DateTimeField(auto_now_add=True, verbose_name='Время последеней попытки')
+    status = models.BooleanField(verbose_name='Статус попытки')
+    server_response = models.CharField(max_length=150, verbose_name='Ответ сервера', **NULLABLE)
+    mailing = models.ForeignKey('Mailing', on_delete=models.CASCADE, verbose_name='Рассылка', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.time}'
+
+    class Meta:
+        """Класс отображения метаданных"""
+        verbose_name = 'Лог'
+        verbose_name_plural = 'Логи'
